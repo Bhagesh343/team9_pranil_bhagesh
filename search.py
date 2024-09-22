0dc0187899f7d7ad85b7e3793e27d81f3519e80a
Bhagesh343 <bhageshbandi39@gmail.com>
2024-09-22 11:33:02 +0530

    added search.py

---
+++ search.py
@@ -0,0 +1,21 @@
+import re
+
+class Search:
+    def _init_(self, filename='default_text.txt'):
+        self.filename = filename
+        try:
+            with open(self.filename, 'r') as file:
+                self.lines = file.readlines()
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File '{self.filename}' not found.")
+
+    def clean(self):
+        self.lines = [re.sub(r'[^a-zA-Z0-9\s]', '', line) for line in self.lines]
+
+
+    def getLines(self, pattern):
+        result = [pattern]
+        for index, line in enumerate(self.lines, start=1):
+            if pattern in line:
+                result.append((index, line.strip()))
+        return result
